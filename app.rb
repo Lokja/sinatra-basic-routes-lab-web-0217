89eb06298b1435444be6e0335c11aeb3b8d83751
Devin Rieger <dev0089@gmail.com>
2017-02-19 16:00:18 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Devin"
+  end
+
+  get '/hometown' do
+    "My hometown is Southampton, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Nessun Dorma performed by Pavarotti"
+  end
+
 end
